b26b68ae46c3560d6b29d338a4551757174a9fc6
남혁준 <hyukjun@namhyeogjun-ui-MacBookAir.local>
2022-03-13 17:12:57 +0900

    update js

---
+++ javascript-practice/switchToIf.js
@@ -0,0 +1,11 @@
+'use strict';
+
+if (browser == 'Edge') {
+    alert( "You are using Edge" );
+}else if(browser==Chrome){
+    alert("You Chrome");
+}else if(browser==Firefox){
+    alert("You Firefox")
+}else {
+    alert("You Are Good");
+}
