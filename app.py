2edc80a8d275c5e54367d68a0503e97119aa07f2
남혁준 <hyukjun@namhyeogjun-ui-MacBookAir.local>
2022-09-07 22:09:42 +0900

    update css, html

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from urllib import response
 from flask import Flask,render_template,request,make_response
 
-import requests
-import os
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    
-    return render_template('./index.html', serverInfo="hello2", ClientInfo="world")
+    return render_template('index.html', serverInfo="hello2", ClientInfo="world")
 
 if __name__ == "__main__":
     app.run()
