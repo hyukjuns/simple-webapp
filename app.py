dcb4016864e10f7817727ab5f7ce9a2de69d83e1
hyukjuns <hyukjuner@gmail.com>
2023-05-31 18:54:50 +0900

    update flask app

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from urllib import response
-from flask import Flask,render_template,request,make_response
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template('index.html', serverInfo="hello2", ClientInfo="world")
+@app.route('/')
+def index():
+    # Get client request header
+    client_request_header = request.headers
 
-if __name__ == "__main__":
+    # Get server information
+    server_info = {
+        'Server': request.host,
+        'Host': request.host_url,
+        'Remote Addr': request.remote_addr
+    }
+
+    # Set response headers
+    response_headers = {
+        'Content-Type': 'text/html',
+        'Custom-Header': 'Custom Value'
+    }
+
+    return render_template('index.html', client_request_header=client_request_header, server_info=server_info, response_headers=response_headers)
+
+# default port=5000
+if __name__ == '__main__':
     app.run()
